b068d2d2f231fef96cc668b110636370e001ee7d
Muhammadali <mshukrulloyev2021@gmail.com>
2023-03-21 23:03:28 +0500

    slider changed to touch slider

--- portfolio/webflow2/script.js
+++ portfolio/webflow2/script.js
@@ -5,13 +5,14 @@ const boxes = document.querySelector('.boxes'),
     parent = document.querySelector('.dots');
 
 let offset = 1, defaultWidth, itemWidth, boxesWidth;
+
 if (window.outerWidth <= 624) {
     defaultWidth = 0;
     itemWidth = wrapper.offsetWidth;
-    boxesWidth = (itemWidth + 30);
+    boxesWidth = itemWidth;
 } else {
-    defaultWidth = ((width / 2) - (box[0].offsetWidth / 2)) * 1;
-    boxesWidth = (box[0].offsetWidth + 30);
+    defaultWidth = (width / 2) - (box[0].offsetWidth / 2);
+    boxesWidth = box[0].offsetWidth - 20;
 }
 
 box.forEach(item => {
@@ -25,28 +26,69 @@ boxes.style.width = box[0].offsetWidth * box.length + "%";
 
 const btn = document.querySelectorAll('.dot');
 
-function refresh() {
-    boxes.style.paddingLeft = defaultWidth + "px";
-    boxes.style.transform = `translateX(-${boxesWidth * offset}px)`;
+function refreshDate() {
+    wrapper.style.padding = `0 ${defaultWidth}px`;
+    wrapper.scrollLeft = boxesWidth * offset;
+    console.log(boxesWidth)
     btn[offset].classList.add('active');
     box[offset].classList.add('active_box');
 }
 
-refresh();
+refreshDate();
+
+function onDeleteActive() {
+    btn.forEach(bt => bt.classList.remove('active'))
+    box.forEach(bt => bt.classList.remove('active_box'))
+}
+
 
 btn.forEach((item, i) => {
     item.addEventListener('click', () => {
-        btn.forEach(bt => bt.classList.remove('active'))
-        box.forEach(bt => bt.classList.remove('active_box'))
+        onDeleteActive();
         offset = i;
-        refresh();
+        refreshDate();
     })
 })
 
 window.addEventListener('keydown', (event) => {
     if (event.key === 'ArrowRight' && offset < box.length - 1) offset++
     if (event.key === 'ArrowLeft' && offset > 0) offset--
-    btn.forEach(bt => bt.classList.remove('active'))
-    box.forEach(bt => bt.classList.remove('active_box'))
-    refresh();
+    onDeleteActive();
+    refreshDate();
+})
+
+// touch
+
+let pressed = false
+let startX = 0
+
+wrapper.addEventListener('mousedown', function (e) {
+    pressed = true
+    startX = e.clientX
+    this.style.cursor = 'grabbing'
+})
+
+wrapper.addEventListener('mouseleave', function (e) {
+    pressed = false
+})
+
+window.addEventListener('mouseup', function (e) {
+    pressed = false
+    wrapper.style.cursor = 'grab'
+})
+
+wrapper.addEventListener('mousemove', function (e) {
+    if (!pressed) {
+        return
+    }
+
+    this.scrollLeft += startX - e.clientX;
+})
+
+window.addEventListener('mouseover', () => {
+    // -- for dot
+    box.forEach((item, i) => item.offsetLeft <= wrapper.scrollLeft + box[0].offsetLeft ? offset = i : null)
+    onDeleteActive();
+    btn[offset].classList.add('active');
+    box[offset].classList.add('active_box');
 })
