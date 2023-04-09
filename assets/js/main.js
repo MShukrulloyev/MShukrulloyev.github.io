4d4ccd5a258f78b768f2580902d6dad787a8bb1c
Muhammadali <mshukrulloyev2021@gmail.com>
2023-04-09 17:12:33 +0500

    img

--- assets/js/main.js
+++ assets/js/main.js
@@ -179,6 +179,7 @@ const sr = ScrollReveal({
     delay: 400,
     // reset: true /* Animations repeat */
 })
+
 sr.reveal(`.home__data, .projects__container, .testimonial__container, .footer__container`)
 sr.reveal(`.home__info div`, { delay: 600, origin: 'bottom', interval: 100 })
 sr.reveal(`.skills__content:nth-child(1), .contact__content:nth-child(1)`, { origin: 'left' })
